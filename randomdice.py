ff61dd9a9a46c4e1a77c3609d587187d308dd3b1
DRoberts487 <146487048+DRoberts487@users.noreply.github.com>
2023-10-04 14:58:13 -0500

    Update randomdice.py

--- randomdice.py
+++ randomdice.py
@@ -7,5 +7,11 @@ for rolls in range (20):
         print ('Critical Hit')
     if randomDice==1:
         print ('YOU LOSE,NICE TRY')
+    if randomDice==7:
+        print ('Roll Again')
+    if randomDice==1+1:
+        print ('Hello')
+    if randomDice==4*4:
+        print ('Roll 3 Times')
 print ('Done Rolling')
 
