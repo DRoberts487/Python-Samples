bbda6906d060af808efa250d4cb625567b449aad
DRoberts487 <146487048+DRoberts487@users.noreply.github.com>
2023-10-16 14:59:23 -0500

    Add files via upload

---
+++ Dice Solution.py
@@ -0,0 +1,19 @@
+Python 3.11.6 (tags/v3.11.6:8b6ee5b, Oct  2 2023, 14:57:12) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/kingd/First Python Assignment.py
+How many times do you want to roll?6
+How many sides is there?6
+Roll is:
+1
+Critical Miss
+Roll is:
+3
+Roll is:
+6
+Roll is:
+7
+Roll is:
+13
+Roll is:
+17
