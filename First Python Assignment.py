b5d4649a61188cc6754ef3a01082534f48ef0728
DRoberts487 <146487048+DRoberts487@users.noreply.github.com>
2023-10-16 15:06:32 -0500

    Add files via upload

---
+++ First Python Assignment.py
@@ -0,0 +1,23 @@
+import random
+numberRolls=int (input('How many times do you want to roll?'))
+numberSides=int (input ('How many sides is there?'))
+for count in range(numberRolls):
+    throw = random.randrange(20)+1
+    print ('Roll is:')
+    print(throw)
+    if throw==20:
+            print ('Critical Hit')
+    elif throw==1:
+            print ('Critical Miss')
+
+
+
+
+
+#Make sure your rolls and dice sides are integers
+#Ask for the number of rolls
+#Ask for the number of sides
+#roll the dice
+#Output the results to the screen
+#Check for a critical hit and output to the screen if it is a critical hit
+#Check for a critical miss and output to the screen if it is a critical miss
