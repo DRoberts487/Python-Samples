3938df4d8f45590dcf6699e273e3d8c12531b042
DRoberts487 <146487048+DRoberts487@users.noreply.github.com>
2023-10-30 15:48:09 -0500

    Add files via upload

---
+++ Math Operartions.py
@@ -0,0 +1,24 @@
+#class example
+a = 8
+if a > 1:
+    print ('b>1')
+if a == 8:
+    print ('a is 8')
+if a != 0:
+    print ('a is somwthing else')
+
+
+
+
+
+
+
+#easy operations
+c = 17
+e = 20
+if(e < c):
+    print ('e is less than c')
+else:
+    print ('c is greater than c')
+
+
