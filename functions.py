d2090d2fbcf6178e9ba2a03792618321794572ed
DRoberts487 <146487048+DRoberts487@users.noreply.github.com>
2023-11-08 15:36:03 -0600

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,14 @@
+def rooms(room, action):
+    print (room, action)
+kitchen = 'Hello Kitchen'
+kitchenAction = 'This is a butcher knife'
+
+porch = 'Hello Porch'
+porchAction = 'This is a rocking chair and a kitty'
+
+bedroom = 'Hello bedroom'
+bedroomAction = 'This is a tv'
+
+rooms(kitchen, kitchenAction)
+rooms(porch, porchAction)
+rooms(bedroom, bedroomAction)
